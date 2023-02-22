e86c005d83b615f670190c64fe894c9581161c13
Charles Frye <cfrye59@gmail.com>
2023-02-22 22:57:54 +0000

    whitespace

--- lab02/text_recognizer/models/mlp.py
+++ lab02/text_recognizer/models/mlp.py
@@ -19,7 +19,6 @@ class MLP(nn.Module):
         data_config: Dict[str, Any],
         args: argparse.Namespace = None,
     ) -> None:
-
         super().__init__()
         self.args = vars(args) if args is not None else {}
         self.data_config = data_config
